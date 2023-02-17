c5ee8300473f20c0bfae66b1164e130455fc957e
Michael Tenu <michten35@gmail.com>
2023-02-17 03:59:18 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - This is the main function of the programme
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
