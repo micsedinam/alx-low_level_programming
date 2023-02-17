c8e543601654d5f242b74a45207a77aba411d195
Michael Tenu <michten35@gmail.com>
2023-02-17 03:34:36 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - this is the main function of the programme
+ *
+ * return: 0 is the end of executable code
+ */
+int main(void)
+{
+	char text[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
+
