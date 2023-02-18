934854c976c9d1ada0e08b034d4bc72a6cbe8223
Michael Tenu <michten35@gmail.com>
2023-02-18 04:51:03 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - this is the main func
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+		putchar('\n');
+		return (0);
+}
