7c00b6a3488cfd5d1b3f76b60cf09e9d4f6bab14
Michael Tenu <michten35@gmail.com>
2023-02-21 22:55:51 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * jack_bauer - is the function we are working with.
+ *
+ * Return; 0
+ */
+
+void jack_bauer(void)
+{
+	int hrs_tens, hrs_ones, mins_tens, mins_ones, hrs_max;
+
+	hrs_max = 58;
+	hrs_tens = '0';
+	while (hrs_tens < '3')
+	{
+		if (hrs_tens == '2')
+		{
+			hrs_max = '4';
+		}
+		hrs_ones = '0';
+		while (hrs_ones < hrs_max)
+		{
+			mins_tens = '0';
+			while (mins_tens < '6')
+			{
+				mins_ones = '0';
+				while (mins_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					mins_ones++;
+				}
+				mins_ones = '0';
+				mins_tens++;
+			}
+			mins_tens = '0';
+			hrs_ones++;
+		}
+		hrs_ones = '0';
+		hrs_tens++;
+	}
+}
