571cc975fc8c70a4085e9d048fe1744cd685e516
Michael Tenu <michten35@gmail.com>
2023-02-23 16:05:15 +0000

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *@c: is a parameter to be checked
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
