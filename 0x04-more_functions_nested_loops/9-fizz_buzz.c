bcf7126171dcdeef7de50cd2126e3c85aafeca47
Michael Tenu <michten35@gmail.com>
2023-02-23 22:38:03 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+                {
+                        printf("FizzBuzz ");
+                }
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -20,17 +24,9 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
 		else
 		{
 			printf("%d ", i);
-		}
-		if (i != 100)
-		{
-			putchar(' ');
 		}
 	}
 	putchar('\n');
