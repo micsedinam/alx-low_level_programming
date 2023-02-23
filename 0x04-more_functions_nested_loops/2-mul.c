a1122bb8d13287c44a4a7ce7062e8b15550677a2
Michael Tenu <michten35@gmail.com>
2023-02-23 16:24:58 +0000

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ * @a: parameter to be checked 
+ * @b: parameter to be checked
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int multiply = a * b;
+	return (multiply);
+}
