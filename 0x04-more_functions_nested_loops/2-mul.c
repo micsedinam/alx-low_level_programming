5f9ff0ca157f5983823083166c2bcb0c1c49f5c0
Michael Tenu <michten35@gmail.com>
2023-02-23 16:37:32 +0000

    Multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * mul - check the code
- * @a: parameter to be checked 
+ * mul - multiplies two numbers
+ * @a: parameter to be checked
  * @b: parameter to be checked
  * Return: Always 0.
  */
 
 int mul(int a, int b)
 {
-	return (a * b);
+	int multiply = a * b;
+
+	return (multiply);
 }
