47d6d83b381ad3d4c3381cb4dbaa3ceffa94d3be
Michael Tenu <michten35@gmail.com>
2023-02-23 19:53:03 +0000

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: variable
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
