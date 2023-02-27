01883fb8fde211c2a05e2370483a40f0d288025b
Michael Tenu <michten35@gmail.com>
2023-02-27 19:03:56 +0000

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - check the code 
- *
+ * reset_to_98 - check the code 
+ * @*n - is the parameter to be checked
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	*n = 98; 
+	*n = 98;
 }
