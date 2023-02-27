5cd69133ccaf237d2e207668ade40bda22137072
Michael Tenu <michten35@gmail.com>
2023-02-27 21:37:29 +0000

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,25 +8,14 @@
  */
 void puts_half(char *str)
 {
-	int len = 0;
+	int i;
 
-	int i, n;
-
-	while (*(str + len) != '\0')
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		len++;
-	}
-	if (len % 2 != 0)
-	{
-		n = (len - 1) / 2;
-	}
-	else
-	{
-		n = len / 2;
-	}
-	for (i = n; i < len; i++)
-	{
-		putchar(*(str + i));
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
