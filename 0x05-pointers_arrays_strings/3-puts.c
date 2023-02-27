58044cd22cf534574190f0236f0d0ab9603e32cc
Michael Tenu <michten35@gmail.com>
2023-02-27 21:09:35 +0000

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string and a new line
+ * @str: pointer parameter to a string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
