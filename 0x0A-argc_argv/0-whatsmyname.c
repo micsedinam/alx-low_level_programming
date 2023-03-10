4ad06f25547465d50afff3243fbafbe2cba0e803
Michael Tenu <michten35@gmail.com>
2023-03-10 23:40:56 +0000

    What is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int  argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 
 int _putchar(char c);
-int main(int __attribute__((__unused__)) argc, char *argv[])
+
 #endif /* MAIN_H */
