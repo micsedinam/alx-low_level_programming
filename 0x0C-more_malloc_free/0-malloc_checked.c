3a546c797827ce18db7c741bb342124745a5bbf6
Michael Tenu <michten35@gmail.com>
2023-03-17 04:57:00 +0000

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function
+ *
+ * @b: param1
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
