79c654d27ff90eaec075e1de96d5b865139d6a19
Michael Tenu <michten35@gmail.com>
2023-04-13 08:36:30 +0000

    Task 9 solution

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,35 +1,43 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - ...
- *
- * @head: ...
- * @idx: ...
- * @n: ...
- * Return: ...
+ * insert_nodeint_at_index - adds a new node to a linked list
+ *                           at a given position
+ * @head : pointer to the head of the list
+ * @idx  : index of the list where the new node will be added
+ *         (indices start at 0)
+ * @n    : value for the new node to be added
+ * Return: pointer to the new node,
+ *         or NULL - if function fails
  */
-listint_t *insert_nodeint_at_index(listint_t **head,
-unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *newNode;
-listint_t *temp = *head;
-unsigned int pos = 0;
+listint_t *new, *temp = *head;
+unsigned int node;
 
+new = malloc(sizeof(listint_t));
 
-
-newNode = (listint_t *) malloc(sizeof(listint_t));
-if (newNode == NULL)
-{
+if (new == NULL)
 return (NULL);
+
+new->n = n;
+
+if (idx == 0)
+{
+new->next = temp;
+*head = new;
+return (new);
 }
 
-while (pos < idx - 1)
+for (node = 0; node < (idx - 1); node++)
 {
+if (temp == NULL || temp->next == NULL)
+return (NULL);
+
 temp = temp->next;
-pos++;
 }
-newNode->n = n;
-newNode->next = temp->next;
-temp->next = newNode;
-return (temp->next);
+
+new->next = temp->next;
+temp->next = new;
+
+return (new);
 }
