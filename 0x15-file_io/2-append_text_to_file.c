6688311e82d17af2e9975ca2da0f7a5231fb061b
Michael Tenu <michten35@gmail.com>
2023-04-15 04:24:12 +0000

    append text to file task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,27 +9,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	unsigned int len = 0;
-	unsigned int write_size;
+	int o, w, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
-	while (*(text_content + len) != '\0')
-		len++;
-
-	write_size = write(fd, text_content, len);
-	if (write_size != len)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(fd);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }
